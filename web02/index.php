bebffc1b6a19c73bd8203885dd3f4853b3752430
applepine07 <mrchildrenmissadult@hotmail.com>
2022-02-20 15:35:04 +0800

    乙二

--- web02/index.php
+++ web02/index.php
@@ -1,5 +1,7 @@
-﻿<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php
+include_once "base.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -13,20 +15,13 @@
 </head>
 
 <body>
-	<div id="alerr"
-		style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 		<pre id="ssaa"></pre>
 	</div>
 	<div id="all">
-		<div id="title">
-			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36
-			<a href="index.php" style="float:right">回首頁</a> 
-		</div>
-		<div id="title2">
-			<a href="index.php" title="健康促進網-回首頁">
-				<img src="./icon/02B01.jpg">
-			</a>
-		</div>
+		<?php
+		include "./front/header.php";
+		?>
 		<div id="mm">
 			<div class="hal" id="lef">
 				<a class="blo" href="?do=po">分類網誌</a>
@@ -37,20 +32,28 @@
 			</div>
 			<div class="hal" id="main">
 				<div>
-					<marquee behavior="" direction="">請民眾踴躍投稿電子報，讓電子報成為大家相交流、分享的園地!詳見最新文章。</marquee>
-					<span style="width:18%; display:inline-block;">
-						<a href="?do=login">會員登入</a>
+					<marquee style="width:78%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相交流、分享的園地!詳見最新文章。</marquee>
+					<span style="width:18%; display:inline-block;text-align:center"><a href="?do=login">會員登入</a>
 					</span>
 					<div class="">
+						<?php
+						$do=$_GET['do']??'home';
+						$file='front/'.$do.".php";
+						if(file_exists($file)){
+							include $file;
+						}else{
+							include "front/home.php";
+						}
+						?>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div id="bottom">
-			本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2022健康促進網社群平台 All Right Reserved
-			<br>
-			服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
-		</div>
+		
+		<?php
+		include "front/footer.php";
+		?>
+
 	</div>
 
 </body>
