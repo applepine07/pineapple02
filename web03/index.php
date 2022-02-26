f46c2931e4f46ac82d461b7878011dbd6a2ed8c7
applepine07 <mrchildrenmissadult@hotmail.com>
2022-02-26 21:06:09 +0800

    基本切版

--- web03/index.php
+++ web03/index.php
@@ -19,7 +19,7 @@
       <a href="index.php">首頁</a>
       <a href="03P02.htm">線上訂票</a>
       <a href="#">會員系統</a>
-      <a href="03P03.htm">管理系統</a>
+      <a href="./back.php">管理系統</a>
     </div>
     <div id="text"> <span class="ct">最新活動</span>
       <marquee direction="right">
@@ -27,28 +27,15 @@
       </marquee>
     </div>
     <div id="mm">
-      <div class="half" style="vertical-align:top;">
-        <h1>預告片介紹</h1>
-        <div class="rb tab" style="width:95%;">
-          <div id="abgne-block-20111227">
-            <ul class="lists">
-            </ul>
-            <ul class="controls">
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="half">
-        <h1>院線片清單</h1>
-        <div class="rb tab" style="width:95%;">
-          <table>
-            <tbody>
-              <tr> </tr>
-            </tbody>
-          </table>
-          <div class="ct"> </div>
-        </div>
-      </div>
+      <?php
+      $do=$_GET['do']??'main';
+      $file="./front/".$do.".php";
+      if(file_exists($file)){
+        include $file;
+      }else{
+        include "./front/main.php";
+      }
+      ?>
     </div>
     <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
   </div>
